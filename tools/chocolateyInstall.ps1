cc094e8fc8ae84e98081b1732faa2e7a1b755ba6
William Gathoye <william@gathoye.be>
2017-10-03 21:58:46 +0200

    Upgrade to 1.5.6 (auto update test)

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,20 +1,20 @@
-$packageName= 'keeweb'
+﻿$packageName= 'keeweb'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/keeweb/keeweb/releases/download/v1.5.5/KeeWeb-1.5.5.win.ia32.exe'
-$url64      = 'https://github.com/keeweb/keeweb/releases/download/v1.5.5/KeeWeb-1.5.5.win.x64.exe'
+$url        = 'https://github.com/keeweb/keeweb/releases/download/v1.5.6/KeeWeb-1.5.6.win.ia32.exe'
+$url64bit   = 'https://github.com/keeweb/keeweb/releases/download/v1.5.6/KeeWeb-1.5.6.win.x64.exe'
 
 $packageArgs = @{
     packageName   = $packageName
     unzipLocation = $toolsDir
     fileType      = 'exe'
     url           = $url
-    url64bit      = $url64
+    url64bit      = $url64bit
 
     softwareName  = 'keeweb*'
 
-    checksum      = '720a26f858000fe1fcd92be3ea375a784074412b37b030b2691c00198022dfee'
+    checksum      = '385176064f55ff30efd543cff624c3548d1bb2eabab6a00625dcc517dc2960ab'
     checksumType  = 'sha256'
-    checksum64    = '5367c5f9e37d747ec6b712cccf979a5b5ee8848280b6e554ad0fe7465d525959'
+    checksum64    = '6413145f57316b9405200e100f9d4dd26874632684cca361bb5f2e7a9b204d60'
     checksumType64= 'sha256'
 
     #NSIS
