9b2f527c9119ff8e4db04f6dcbc7e14f1cd910c0
William Gathoye <william@gathoye.be>
2018-05-20 08:34:32 -0700

    Upgrade to 1.6.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName= 'keeweb'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/keeweb/keeweb/releases/download/v1.6.0/KeeWeb-1.6.0.win.ia32.exe'
-$url64bit   = 'https://github.com/keeweb/keeweb/releases/download/v1.6.0/KeeWeb-1.6.0.win.x64.exe'
+$url        = 'https://github.com/keeweb/keeweb/releases/download/v1.6.1/KeeWeb-1.6.1.win.ia32.exe'
+$url64bit   = 'https://github.com/keeweb/keeweb/releases/download/v1.6.1/KeeWeb-1.6.1.win.x64.exe'
 
 $packageArgs = @{
     packageName   = $packageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
     softwareName  = 'keeweb*'
 
-    checksum      = '17587ac147d791ea873d1717e02d46fb5a3c57750b8496a67e49656bf2a025e0'
+    checksum      = 'cad99d525629bb2d4cc41e1b36b0bafed92afc060c03a7e840585c4098cd99be'
     checksumType  = 'sha256'
-    checksum64    = '9bfcae5f9a15daa0183425f667d956f20795d9d50da0d34ceac94a0e8319982e'
+    checksum64    = 'c9146e73e99595e9d9b5bb77ff978d2777c4ba19530da9d48d62c74ed619958e'
     checksumType64= 'sha256'
 
     #NSIS
